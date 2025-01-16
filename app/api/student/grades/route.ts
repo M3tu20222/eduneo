83b33bbfd7603f4cd76d60bf136bbd8000de5159
M3tu20222 <memesis666@gmail.comm>
2025-01-16 22:00:00 +0300

    asignmdersler recharts

--- app/api/student/grades/route.ts
+++ app/api/student/grades/route.ts
@@ -7,10 +7,6 @@ import Course from "@/models/Course";
 import User, { IUser } from "@/models/User";
 import { Types, Document } from "mongoose";
 
-interface IUserWithCourses extends IUser, Document {
-  courses: ICourse[];
-}
-
 interface ICourse {
   _id: Types.ObjectId;
   name: string;
@@ -28,6 +24,10 @@ interface IGrade {
   date: Date;
 }
 
+type IUserWithCourses = Omit<IUser, "courses"> & {
+  courses: ICourse[];
+} & Document;
+
 export async function GET(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
@@ -40,9 +40,9 @@ export async function GET(req: NextRequest) {
 
     const userId = session.user.id;
 
-    const student = (await User.findById(userId).populate<{
-      courses: ICourse[];
-    }>("courses")) as IUserWithCourses | null;
+    const student = (await User.findById(userId)
+      .populate<{ courses: ICourse[] }>("courses")
+      .lean()) as IUserWithCourses | null;
     if (!student) {
       return NextResponse.json(
         { error: "Öğrenci bulunamadı" },
@@ -50,10 +50,9 @@ export async function GET(req: NextRequest) {
       );
     }
 
-    const courseIds =
-      student?.courses.map((course: ICourse) => course._id) || [];
+    const courseIds = student.courses.map((course: ICourse) => course._id);
 
-    const grades = await Grade.find({
+    const grades = (await Grade.find({
       student: userId,
       course: { $in: courseIds },
     })
@@ -66,29 +65,26 @@ export async function GET(req: NextRequest) {
         },
       })
       .sort("course")
-      .lean();
+      .lean()) as IGrade[];
 
-    const formattedGrades = grades.reduce<Record<string, any>>(
-      (acc, grade: IGrade) => {
-        const courseId = grade.course._id.toString();
-        if (!acc[courseId]) {
-          acc[courseId] = {
-            courseId: courseId,
-            courseName: grade.course.name,
-            teacherName: `${grade.course.teacher.firstName} ${grade.course.teacher.lastName}`,
-            grades: [],
-          };
-        }
-        acc[courseId].grades.push({
-          _id: grade._id.toString(),
-          type: grade.type,
-          value: grade.value,
-          date: grade.date.toISOString(),
-        });
-        return acc;
-      },
-      {}
-    );
+    const formattedGrades = grades.reduce<Record<string, any>>((acc, grade) => {
+      const courseId = grade.course._id.toString();
+      if (!acc[courseId]) {
+        acc[courseId] = {
+          courseId: courseId,
+          courseName: grade.course.name,
+          teacherName: `${grade.course.teacher.firstName} ${grade.course.teacher.lastName}`,
+          grades: [],
+        };
+      }
+      acc[courseId].grades.push({
+        _id: grade._id.toString(),
+        type: grade.type,
+        value: grade.value,
+        date: grade.date.toISOString(),
+      });
+      return acc;
+    }, {});
 
     return NextResponse.json(Object.values(formattedGrades));
   } catch (error) {
